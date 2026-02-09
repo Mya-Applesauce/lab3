c7fb06e81832e8f4db51bf2c54d5e1586e69caa3
Ari Papke <myaapplesauce42@gmail.com>
2026-02-09 12:30:27 -0500

    Add files via upload

---
+++ lab3_apapke_add.py
@@ -0,0 +1,14 @@
+"""
+Lab3_apapke_list
+Ari Papke
+List addition
+02/09/26
+"""
+
+from lab3_apapke_list import sonic_characters
+
+sonic_characters.extend(('espio', 'vector', 'charmy', 'tangle', 'whisper'))
+sonic_characters.sort()
+sonic_characters.reverse()
+
+print(sonic_characters)
