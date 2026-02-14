363904f840379a9338a195f6e52d17798d11b13e
Ari Papke <myaapplesauce42@gmail.com>
2026-02-13 21:48:33 -0500

    Add files via upload

---
+++ lab3_apapke_replace.py
@@ -0,0 +1,13 @@
+"""
+Lab3_apapke_replace
+Ari Papke
+List replacement
+02/09/26
+"""
+
+from lab3_apapke_add import sonic_characters
+
+sonic_characters[7] = "binoculars"
+
+print(f"{sonic_characters[0:7]} \n {sonic_characters[7]} \n {sonic_characters[8:15]}")
+
