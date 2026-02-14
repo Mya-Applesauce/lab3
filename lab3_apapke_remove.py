8594ae5439c44d68ffabb59ccf97accdcd11ba0f
Ari Papke <myaapplesauce42@gmail.com>
2026-02-13 21:56:42 -0500

    Add files via upload

---
+++ lab3_apapke_remove.py
@@ -0,0 +1,13 @@
+"""
+Lab3_apapke_remove
+Ari Papke
+List removement
+02/09/26
+"""
+
+from lab3_apapke_replace import sonic_characters
+
+sonic_characters.remove("binoculars")
+
+print(sonic_characters)
+print(f"{len(sonic_characters)}")
